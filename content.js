671392313cbf905dffc1bfa0d76a466c46480bbf
Brian Purgert <brianpurgert2@gmail.com>
2019-12-05 05:06:18 -0500

    Initial commit

--- content.js
+++ content.js
@@ -1,10 +1,3 @@
-import App from './App.vue'
-
-const sidebar = '#root > div > div.tw-flex.tw-flex-column.tw-flex-nowrap.tw-full-height > div > div.side-nav.tw-c-background-alt.tw-flex-shrink-0.tw-full-height > div > div.side-nav__overlay-wrapper.tw-flex.tw-full-height.tw-overflow-hidden.tw-relative > div > div.simplebar-scroll-content > div > div > div:nth-child(2) > div.tw-flex-grow-1 > div:nth-child(1) > div.tw-relative.tw-transition-group'
-const username = '[data-a-target="user-display-name"]'
-let mixerSidebarParent = document.querySelector('b-channel-page-wrapper > b-channel-web-page')
-
-
 function htmlToElement (html) {
 	let template = document.createElement('template')
 	html = html.trim()
@@ -12,63 +5,116 @@ function htmlToElement (html) {
 	return template.content.firstChild
 }
 
+function mountMixerSidebar() {
+	let mixerSidebar = htmlToElement(`<div id="mixer-side-nav" class=""><div class="side-nav-header"><h5>Followed Channels</h5></div><div id="live-channels"></div></div>`)
+	let mixerSidebarParent = document.querySelector('b-app .content')
+	mixerSidebarParent.appendChild(mixerSidebar)
+}
 
-async function channelDetails (name) {
-	const url = `https://mixer.com/api/v1/channels/${name}`
-	const response = await fetch(url)
-	const details = await response.json()
-	if (details.online) {
-		addStreamerElement(details.token, `https://mixer.com/${details.token}`, details.user.avatarUrl, details.name, details.viewersCurrent)
+function twitchStreams () {
+	chrome.storage.sync.get(['twitch'],async function(details) {
+		if (!chrome.runtime.error) {
+			let url    = `https://api.twitch.tv/helix/streams?from_id=${details.twitch.id}`
+			let response = await fetch(url,{
+				headers: {
+					'Client-ID': 'yr67fty5tcazl2ew3jda8sw17cy87d'
+				},
+			})
+			let details  = await response.json().data
+		}
+	})
+}
+
+async function mixerStreams (userId) {
+				let url      = `https://mixer.com/api/v1/users/${userId}/follows?fields=id,online,name,token,viewersCurrent,user&where=online:eq:true&order=viewersCurrent:desc&limit=100&page=0`
+				let response = await fetch(url)
+				let details  = await response.json()
+				details.forEach(streamer => {
+					console.log(streamer)
+					addStreamerElement(
+						streamer.token,
+						`https://mixer.com/${streamer.token}`,
+						streamer.user.avatarUrl,
+						streamer.name,
+						streamer.viewersCurrent
+					)
+				})
+}
+
+// async function channelDetails (name) {
+// 	const url = `https://mixer.com/api/v1/channels/${name}`
+// 	const response = await fetch(url)
+// 	const details = await response.json()
+// 	if (details.online) {
+// 		addStreamerElement(
+// 			details.token,
+// 			`https://mixer.com/${details.token}`,
+// 			details.user.avatarUrl,
+// 			details.name,
+// 			details.viewersCurrent
+// 		)
+// 	}
+// }
+
+// function listElementsExist() {
+// 	return !!([...document.querySelector('.side-nav-header').nextSibling.childNodes])
+// }
+
+// function getElements(cb) {
+// 	const interval = setInterval(() => {
+// 		if (listElementsExist()) {
+// 			clearInterval(interval)
+// 			cb([...document.querySelector('.side-nav-header').nextSibling.childNodes])
+// 		}
+// 	}, 1000)
+// }
+
+// getElements((els) => {
+// 	console.log(els) // here do the map thing from above
+// 	const out = els.map((el) => {
+// 		const [channelName, game, viewerCount] = el.innerText.split('\n\n')
+// 		return {channelName, game, viewerCount}
+// 	})
+// 	console.log(out)
+// })
+
+// let openNewTab = 'rel="noopener noreferrer" target="_blank"'
+let openNewTab = ''
+function addStreamerElement (channelName, href, src, game, viewerCount) {
+	let followedStreamerTemplate = htmlToElement(`<div class="tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done" style="transition-property: transform, opacity; transition-timing-function: ease;"><div><div class="side-nav-card tw-align-items-center tw-flex tw-relative" style="background-color: #212c3d" ><a ${openNewTab} class="side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05" data-a-id="followed-channel-0" data-a-target="followed-channel" href="${href}"><div class="side-nav-card__avatar tw-align-items-center tw-flex-shrink-0"><figure aria-label="${channelName}" class="tw-avatar tw-avatar--size-30"><img class="tw-block tw-border-radius-rounded tw-image tw-image-avatar" alt="${channelName}" src="${src}"></figure></div><div class="tw-ellipsis tw-flex tw-full-width tw-justify-content-between"><div data-a-target="side-nav-card-metadata" class="tw-ellipsis tw-full-width tw-mg-l-1"><div class="side-nav-card__title tw-align-items-center tw-flex"><p class="tw-c-text-alt tw-ellipsis tw-ellipsis tw-flex-grow-1 tw-font-size-5 tw-line-height-heading tw-semibold" data-a-target="side-nav-title" title="${channelName}">${channelName}</p></div><div data-a-target="side-nav-game-title" class="side-nav-card__metadata tw-pd-r-05"><p class="tw-c-text-alt-2 tw-ellipsis tw-font-size-6 tw-line-height-heading" title="${game}">${game}</p></div></div><div data-a-target="side-nav-live-status" class="side-nav-card__live-status tw-flex-shrink-0 tw-mg-l-05"><div class="tw-align-items-center tw-flex"><div style="background-color: #1fbaed" class="tw-border-radius-rounded tw-channel-status-indicator tw-channel-status-indicator--live tw-channel-status-indicator--small tw-inline-block tw-relative"></div><div class="tw-mg-l-05"><span class="tw-c-text-alt tw-font-size-6">${viewerCount}</span></div></div></div></div></a></div></div></div>`)
+
+	if (window.location.hostname === "mixer.com"){
+		let twitchFollowedChannels = document.querySelector('#live-channels')
+		twitchFollowedChannels.appendChild(followedStreamerTemplate)
+	} else if (window.location.hostname === "www.twitch.tv"){
+		let twitchFollowedChannels = document.querySelector('[data-a-target="side-nav-header-expanded"] + div.tw-relative.tw-transition-group')
+		twitchFollowedChannels.appendChild(followedStreamerTemplate)
 	}
 }
 
-function listElementsExist() {
-	return !!([...document.querySelector('.side-nav-header').nextSibling.childNodes])
+function userFollows () {
+	document.body.onload = function () {
+		chrome.storage.sync.get("data", function (items) {
+			if (!chrome.runtime.error) {
+				console.log(items)
+				document.getElementById("data").innerText = items.data
+			}
+		})
+	}
 }
 
-function getElements(cb) {
-	const interval = setInterval(() => {
-		if (listElementsExist()) {
-			clearInterval(interval)
-			cb([...document.querySelector('.side-nav-header').nextSibling.childNodes])
+document.body.onload = function() {
+	chrome.storage.sync.get(['mixer','twitch'], function(details) {
+		if (!chrome.runtime.error) {
+			if (window.location.hostname === "mixer.com"){
+				mountMixerSidebar()
+			}
+
+			mixerStreams(details.mixer.userId)
 		}
-	}, 1000)
-}
-
-getElements((els) => {
-	console.log(els) // here do the map thing from above
-	const out = els.map((el) => {
-		const [channelName, game, viewerCount] = el.innerText.split('\n\n')
-		return {channelName, game, viewerCount}
 	})
-	console.log(out)
-})
-
-function addStreamerElement (channelName, href, src, game, viewerCount) {
-	let followedStreamerTemplate = htmlToElement(`<div class="tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done" style="transition-property: transform, opacity; transition-timing-function: ease;"><div><div class="side-nav-card tw-align-items-center tw-flex tw-relative" style="background-color: #212c3d" ><a rel="noopener noreferrer" target="_blank" class="side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05" data-a-id="followed-channel-0" data-a-target="followed-channel" href="${href}"><div class="side-nav-card__avatar tw-align-items-center tw-flex-shrink-0"><figure aria-label="${channelName}" class="tw-avatar tw-avatar--size-30"><img class="tw-block tw-border-radius-rounded tw-image tw-image-avatar" alt="${channelName}" src="${src}"></figure></div><div class="tw-ellipsis tw-flex tw-full-width tw-justify-content-between"><div data-a-target="side-nav-card-metadata" class="tw-ellipsis tw-full-width tw-mg-l-1"><div class="side-nav-card__title tw-align-items-center tw-flex"><p class="tw-c-text-alt tw-ellipsis tw-ellipsis tw-flex-grow-1 tw-font-size-5 tw-line-height-heading tw-semibold" data-a-target="side-nav-title" title="${channelName}">${channelName}</p></div><div data-a-target="side-nav-game-title" class="side-nav-card__metadata tw-pd-r-05"><p class="tw-c-text-alt-2 tw-ellipsis tw-font-size-6 tw-line-height-heading" title="${game}">${game}</p></div></div><div data-a-target="side-nav-live-status" class="side-nav-card__live-status tw-flex-shrink-0 tw-mg-l-05"><div class="tw-align-items-center tw-flex"><div style="background-color: #1fbaed" class="tw-border-radius-rounded tw-channel-status-indicator tw-channel-status-indicator--live tw-channel-status-indicator--small tw-inline-block tw-relative"></div><div class="tw-mg-l-05"><span class="tw-c-text-alt tw-font-size-6">${viewerCount}</span></div></div></div></div></a></div></div></div>`)
-	let followedChannels = document.querySelector(sidebar)
-	followedChannels.appendChild(followedStreamerTemplate)
 }
 
+console.log(window.location.hostname)
 
 
-// chrome.storage.sync.set({ mytext:  });
-// chrome.storage.sync.get('mytext', function(data) {
-// 	yourTextArea.value = data.mytext;
-// });
-
-
-channelDetails ('ninja')
-channelDetails('NepentheZ')
-channelDetails('shroud')
-
-addMixerSidebar()
-
-
-//On Mixer
-function addMixerSidebar() {
-	let mixerSidebar = htmlToElement(`<aside class="" style="width: 240px;position: absolute;top: 60px;left: 0;bottom: 0;background: #3d2121;"></aside>`)
-
-}
-// Insert sidebar as first child of document.querySelector('b-channel-web-page')
-// add the style margin-left: 240px; to document.querySelector('.channel-page')
--- popup.js
+++ popup.js
@@ -1,4 +1,89 @@
 
-chrome.storage.sync.get('mytext', function(data) {
-	document.querySelector('div').value = data.mytext;
-});
+document.body.onload = function() {
+	chrome.storage.sync.get(['mixer','twitch'], function(details) {
+		if (!chrome.runtime.error) {
+			// document.getElementById("log").textContent = JSON.stringify(details.mixer, undefined, 4)
+			// document.getElementById("log2").textContent = JSON.stringify(details.twitch, undefined, 4)
+
+			let twitchPlaceholder = `${details.twitch.display_name} (${details.twitch.id})`
+			document.getElementById("twitchInput").placeholder = twitchPlaceholder
+			let mixerPlaceholder = `${details.mixer.token} (${details.mixer.userId})`
+			document.getElementById("mixerInput").placeholder = mixerPlaceholder
+		}
+	})
+}
+
+// async function channelDetails (name) {
+// 	const url = `https://mixer.com/api/v1/channels/${name}`
+// 	let response = await fetch(url)
+// 	let details = await response.json()
+// }
+
+// document.getElementById("mixerInput").addEventListener("keydown", function(event) {
+// 	event.preventDefault();
+// 	if (event.key === 'Enter') {
+// 		document.getElementById("mixerUpdate").click();
+// 	}
+// })
+
+document.getElementById("mixerUpdate").onclick = updateMixer
+
+async function updateMixer() {
+	let token    = document.getElementById("mixerInput").value
+	const url    = `https://mixer.com/api/v1/channels/${token}`
+	let response = await fetch(url)
+	let details  = await response.json()
+	// chrome.storage.local.clear()
+	chrome.storage.sync.set({"mixer": details}, function () {
+		if (chrome.runtime.error) {
+			console.log("Runtime error.")
+		}
+	})
+}
+
+document.getElementById("twitchUpdate").onclick = updateTwitch
+// https://api.twitch.tv/helix/streams?from_id=57102172
+
+async function updateTwitch() {
+	let login    = document.getElementById("twitchInput").value
+	let url    = `https://api.twitch.tv/helix/users?login=${login}`
+	let response = await fetch(url,{
+		headers: {
+			'Client-ID': 'yr67fty5tcazl2ew3jda8sw17cy87d'
+		},
+	})
+	let details  = await response.json()
+	document.getElementById("log2").textContent = JSON.stringify(details, undefined, 4)
+	chrome.storage.sync.set({"twitch": details.data[0]}, function () {
+		if (chrome.runtime.error) {
+			console.log("Runtime error.")
+		}
+	})
+}
+
+
+
+// const a = `https://api.twitch.tv/kraken/streams/followed?limit=100&offset=0`
+
+// const b = `https://mixer.com/api/v1/users/search?query=${query}`
+// let userId = ""
+	// `https://mixer.com/api/v1/users/current`
+	// `https://mixer.com/api/v1/users/${userId}/follows?fields=id,token&limit=100&page=0`
+	// `https://mixer.com/api/v1/users/${userId}/follows?fields=id,online,name,token,viewersCurrent,partnered,costreamId,interactive,type,audience,user&where=online:eq:true&order=viewersCurrent:desc&limit=100&page=0`
+// const c = `https://mixer.com/api/v1/users/${userId}/follows?limit=100&page=0&where=online:eq:true`
+
+
+async function channelDetails (name) {
+	const url = `https://mixer.com/api/v1/channels/${name}`
+	const response = await fetch(url)
+	const details = await response.json()
+	if (details.online) {
+		addStreamerElement(
+			details.token,
+			`https://mixer.com/${details.token}`,
+			details.user.avatarUrl,
+			details.name,
+			details.viewersCurrent
+		)
+	}
+}
