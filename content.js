66ef7b67dcf594f8498cb19c89f0250b1a5053c2
Brian Purgert <brianpurgert2@gmail.com>
2019-12-06 02:13:38 -0500

    Added Favorites Checkbox Added Twixer-Roobert fonts

--- content.js
+++ content.js
@@ -78,10 +78,44 @@ async function mixerStreams (userId) {
 // 	console.log(out)
 // })
 
+function streamerTemplate(channelName, href, src, game, viewerCount){
+	let isMixer = true
+
+	const template = `<div class="tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done" style="transition-property: transform, opacity; transition-timing-function: ease;">
+    <div>
+        <div class="side-nav-card tw-align-items-center tw-flex tw-relative" style="background-color: #212c3d"><a ${openNewTab} class="side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05" data-a-id="followed-channel-0" data-a-target="followed-channel" href="${href}">
+            <div class="side-nav-card__avatar tw-align-items-center tw-flex-shrink-0">
+                <figure aria-label="${channelName}" class="tw-avatar tw-avatar--size-30"><img class="tw-block tw-border-radius-rounded tw-image tw-image-avatar" alt="${channelName}" src="${src}"></figure>
+            </div>
+            <div class="tw-ellipsis tw-flex tw-full-width tw-justify-content-between">
+                <div data-a-target="side-nav-card-metadata" class="tw-ellipsis tw-full-width tw-mg-l-1">
+                    <div class="side-nav-card__title tw-align-items-center tw-flex">
+                    	<p class="tw-c-text-alt tw-ellipsis tw-ellipsis tw-flex-grow-1 tw-font-size-5 tw-line-height-heading tw-semibold" data-a-target="side-nav-title" title="${channelName}">${channelName}</p>
+                    </div>
+                    <div data-a-target="side-nav-game-title" class="side-nav-card__metadata tw-pd-r-05">
+                    	<p class="tw-c-text-alt-2 tw-ellipsis tw-font-size-6 tw-line-height-heading" title="${game}">${game}</p>
+                    </div>
+                </div>
+                <div data-a-target="side-nav-live-status" class="side-nav-card__live-status tw-flex-shrink-0 tw-mg-l-05">
+                    <div class="tw-align-items-center tw-flex">
+                        <div style="background-color: ${ isMixer ? '#1fbaed' : '#1fbaed' }" class="tw-border-radius-rounded tw-channel-status-indicator tw-channel-status-indicator--live tw-channel-status-indicator--small tw-inline-block tw-relative"></div>
+                        <div class="tw-mg-l-05"><span class="tw-c-text-alt tw-font-size-6">${viewerCount}</span></div>
+                    </div>
+                    <div class="favorite-checkbox">
+                    	<input class="star" type="checkbox" title="Favorite"/>
+					</div>
+                </div>
+            </div>
+        </a></div>
+    </div>
+</div>`
+return template
+}
+
 // let openNewTab = 'rel="noopener noreferrer" target="_blank"'
 let openNewTab = ''
 function addStreamerElement (channelName, href, src, game, viewerCount) {
-	let followedStreamerTemplate = htmlToElement(`<div class="tw-transition tw-transition--duration-medium tw-transition--enter-done tw-transition__scale-over tw-transition__scale-over--enter-done" style="transition-property: transform, opacity; transition-timing-function: ease;"><div><div class="side-nav-card tw-align-items-center tw-flex tw-relative" style="background-color: #212c3d" ><a ${openNewTab} class="side-nav-card__link tw-align-items-center tw-flex tw-flex-nowrap tw-full-width tw-interactive tw-link tw-link--hover-underline-none tw-pd-x-1 tw-pd-y-05" data-a-id="followed-channel-0" data-a-target="followed-channel" href="${href}"><div class="side-nav-card__avatar tw-align-items-center tw-flex-shrink-0"><figure aria-label="${channelName}" class="tw-avatar tw-avatar--size-30"><img class="tw-block tw-border-radius-rounded tw-image tw-image-avatar" alt="${channelName}" src="${src}"></figure></div><div class="tw-ellipsis tw-flex tw-full-width tw-justify-content-between"><div data-a-target="side-nav-card-metadata" class="tw-ellipsis tw-full-width tw-mg-l-1"><div class="side-nav-card__title tw-align-items-center tw-flex"><p class="tw-c-text-alt tw-ellipsis tw-ellipsis tw-flex-grow-1 tw-font-size-5 tw-line-height-heading tw-semibold" data-a-target="side-nav-title" title="${channelName}">${channelName}</p></div><div data-a-target="side-nav-game-title" class="side-nav-card__metadata tw-pd-r-05"><p class="tw-c-text-alt-2 tw-ellipsis tw-font-size-6 tw-line-height-heading" title="${game}">${game}</p></div></div><div data-a-target="side-nav-live-status" class="side-nav-card__live-status tw-flex-shrink-0 tw-mg-l-05"><div class="tw-align-items-center tw-flex"><div style="background-color: #1fbaed" class="tw-border-radius-rounded tw-channel-status-indicator tw-channel-status-indicator--live tw-channel-status-indicator--small tw-inline-block tw-relative"></div><div class="tw-mg-l-05"><span class="tw-c-text-alt tw-font-size-6">${viewerCount}</span></div></div></div></div></a></div></div></div>`)
+	let followedStreamerTemplate = htmlToElement(streamerTemplate(channelName, href, src, game, viewerCount))
 
 	if (window.location.hostname === "mixer.com"){
 		let twitchFollowedChannels = document.querySelector('#live-channels')
